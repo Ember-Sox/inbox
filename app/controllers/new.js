c0eb71d70d06f7eb377c4859f93205fb95eec1f4
Jake Robbins <jrobbinsd@gmail.com>
2017-11-28 14:38:49 -0700

    its alive!

--- app/controllers/new.js
+++ app/controllers/new.js
@@ -3,25 +3,21 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   actions: {
     addNew: function() {
-      let subject = this.get('subject')
-      let body = this.get('body')
-      let read = this.get('read')
-      let starred = this.get('starred')
-      let labels = this.get('labels')
-      alert(subject)
+      var subject = this.get('subject')
+      var body = this.get('body')
+      var read = false
+      var starred = false
+      var labels = [1, 2, 3]
 
-      var newMessage = this.store.createRecord('message', {
+      var newMessage = this.store.createRecord('messages', {
         subject: subject,
         body: body,
         read: read,
         starred: starred,
         labels: labels,
       })
-
       newMessage.save()
-
-      self.transitionToRoute('/')
-
+      this.transitionToRoute('application')
     }
   }
 });
